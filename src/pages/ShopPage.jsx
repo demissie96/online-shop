fade6a4b3b8f998dc0f38cc87ca576095c306845
Johannes Demissie <demissie.johannes@gmail.com>
2022-09-30 18:12:14 +0200

    Add Header and style shopping cart.

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Header from "./components/Header";
 
 function ShopPage() {
-  return <>
-  <h1>Hello Shopper</h1>
-  </>;
+  return (
+    <>
+      <Header />
+      <h1>Hello Shopper</h1>
+    </>
+  );
 }
 
 export default ShopPage;
--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -1,9 +1,118 @@
 import React from "react";
+import "./Header.css";
 
 function Header() {
-  return <>
-  
-  </>;
+  return (
+    <>
+      <nav
+        className="navbar navbar-dark bg-dark"
+        aria-label="Dark offcanvas navbar"
+      >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/shop">
+            Online Shop
+          </a>
+          <div>
+            <i id="items-in-cart">0</i>
+            <span id="cart-text">Shopping Cart</span>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasNavbarDark"
+              aria-controls="offcanvasNavbarDark"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                class="bi bi-cart"
+                viewBox="0 0 16 16"
+              >
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+              </svg>
+            </button>
+          </div>
+
+          <div
+            className="offcanvas offcanvas-end text-bg-dark"
+            tabindex="-1"
+            id="offcanvasNavbarDark"
+            aria-labelledby="offcanvasNavbarDarkLabel"
+          >
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasNavbarDarkLabel">
+                Offcanvas
+              </h5>
+              <button
+                type="button"
+                className="btn-close btn-close-white"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Link
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <form className="d-flex mt-3" role="search">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button className="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Header;
